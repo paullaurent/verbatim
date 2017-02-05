a62043105653b3212cf634d543e59ac807191778
Paul Laurent <Paul Laurent>
2017-02-05 17:27:12 +0100

    tokenisation ok, corpus non abouti)

--- code/essai/essais.py
+++ code/essai/essais.py
@@ -1,9 +1,20 @@
-def tokenisation (lien):
+def corpus (lien):
+    import nltk
     from nltk.corpus import PlaintextCorpusReader
     tokenizer = nltk.data.load('tokenizers/punkt/french.pickle')
-    from nltk.corpus import BracketParseCorpusReader
-    corpus_root = "lien" 
+    from nltk.corpus import PlaintextCorpusReader
+    corpus_root = lien
     file_pattern = "discours.txt" 
-    ptb = BracketParseCorpusReader(corpus_root, file_pattern)
-    ptb.fileids()
-    return void
+    ptb = PlaintextCorpusReader(corpus_root, file_pattern)
+    print(ptb.fileids())
+    print(ptb.words('discours.txt'))
+    
+    
+def tokenisation (lien):
+    import nltk, re, pprint
+    from nltk import word_tokenize
+    f = open(lien)
+    raw = f.read()
+    liste=word_tokenize(raw)
+    print(liste)
+
