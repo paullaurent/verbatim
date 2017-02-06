b270c85b8c2fe21ba403691d0663bafc4d87aa22
Paul Laurent <Paul Laurent>
2017-02-06 17:34:58 +0100

    les stopwords sont supprimés, début de la recherche de structure syntaxique

--- code/essai/essais.py
+++ code/essai/essais.py
@@ -1,3 +1,4 @@
+
 def corpus (lien):
     import nltk
     from nltk.corpus import PlaintextCorpusReader
@@ -13,18 +14,29 @@ def corpus (lien):
 def tokenisation (lien):
     import nltk, re, pprint
     from nltk import word_tokenize
-    f = open(lien)
-    raw = f.read()
-    liste=word_tokenize(raw)
+    f = open(lien) #ouverture texte
+    raw = f.read()#lecteure texte
+    liste=word_tokenize(raw) #transforme en liste de mots le texte
     print(type(liste))
-    print(liste)
+    #print(liste)
     return liste
-    
-    
+
+liste_ponctuations=[',','?','!',';',':','.']
 def stop_words(liste_mots):
     from nltk.corpus import stopwords
+    liste_propre=[]  #obligé de passer par une liste parallèle car python gère mal la suppression des éléments de la liste lors de son parcours
     for mot in liste_mots: 
-        if mot.lower() in stopwords.words('french'):
-           liste_mots.remove(mot)
-    return liste_mots
+        if mot.lower() not in stopwords.words('french') and mot not in liste_ponctuations:
+           liste_propre.append(mot)
+    return liste_propre
 
+
+def structure(liste):
+    from nltk import tag
+    structurePhrase = tag.pos_tag(liste)
+    return structurePhrase
+
+
+
+liste=tokenisation("C:\\Users\\Paul\\Documents\\ecole\\info\\projetS2\\verbatim\\code\\essai\\discours.txt")
+a=(structure(liste))
